55349df6b16ad9deb5b0e4b2638b5f3ab2588974
PNoryk <pnoryk@mail.ru>
2017-11-15 11:22:34 +0300

    Added view and template for detailed blog post as well as CSS for the site.

--- hypermarket/urls.py
+++ hypermarket/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.product_list, name='product_list'),
+url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- hypermarket/views.py
+++ hypermarket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Product
@@ -9,3 +9,7 @@ def product_list(request):
     products = Product.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'hypermarket/product_list.html', {'posts': products})
 
+
+def post_detail(request, pk):
+    post = get_object_or_404(Product, pk=pk)
+    return render(request, 'hypermarket/post_detail.html', {'post': post})
