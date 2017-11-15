4fd4e81a34458ffd26ecee9f36b66869f1babb8d
PNoryk <pnoryk@mail.ru>
2017-11-15 10:23:24 +0300

    Modified templates to display posts from database.

--- hypermarket/views.py
+++ hypermarket/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
 
+from .models import Product
 # Create your views here.
 
+
 def product_list(request):
-    return render(request, 'hypermarket/product_list.html', {})
+    products = Product.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'hypermarket/product_list.html', {'posts': products})
+
