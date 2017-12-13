360cc761164a6a99a9abcc732753805b6c9ca013
PNoryk <pnoryk@mail.ru>
2017-12-13 11:21:44 +0300

    commit

--- hypermarket/urls.py
+++ hypermarket/urls.py
@@ -1,15 +1,17 @@
-from django.conf import settings
 from django.conf.urls import url
-from django.conf.urls.static import static
+from django.urls import path
+
 from . import views
-from django.views.generic import DetailView
-from .models import Product
 
 urlpatterns = [
-    url(r'^$', views.product_list, name='product_list'),
+    # url(r'^$', views.product_list, name='product_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^aaa$', views.request_str, name='req_str')
+    # url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^get_req_str$', views.request_str, name='req_str'),
+    path("post/new/", views.post_new, name='post_new'),
+    path("home/", views.product_list, name='product_list'),
+    # path("post/((\<pk>)", views.post_detail, name='post_detail')
+
     # url(r'^post/(?P<pk>[0-9]+)/$', DetailView.as_view(
     #     context_object_name="Product",
     #     model=Product,
